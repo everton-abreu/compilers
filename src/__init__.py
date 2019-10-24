02948b0b8770e0c39aa2bf40ed80672a56d5f0aa
Everton Junior de Abreu <everton@jrabreu.com>
2019-10-24 15:07:03 -0300

    reset __init__ ;

--- src/__init__.py
+++ src/__init__.py
@@ -1,22 +1,12 @@
-# __main__.py
+# __init__.py
 
 import sys
+from anytree import RenderTree
+from anytree.exporter import UniqueDotExporter
 from lexer import tokenize
-from syntatic import syntatic
+from syntatic import parse
+from semantic import semantic
 
 if __name__ == "__main__":
-  print("to aqui")
-  if (len(sys.argv) < 2):
-    exit(1)
-
-  file = open(sys.argv[1], 'r')
-  data = file.read()
-
-  logFile = open('logs/' + sys.argv[1].split('.')[0].split('/')[-1] + '.marks', 'w')
-
-  tokens = tokenize(data)
-
-  for token in tokens:
-    logFile.write(str(token) + '\n')
-
-  pass
+  print('um dia eu mexo aqui')
+  exit(0)
